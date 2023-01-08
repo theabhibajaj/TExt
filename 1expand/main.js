2a5aa9ce0ba584f0b4ee60ef03f0db6ad77a0625
theabhibajaj <89036139+theabhibajaj@users.noreply.github.com>
2023-01-08 11:28:20 -0500

    Add files via upload

---
+++ 1expand/main.js
@@ -0,0 +1,38 @@
+results= "";
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet  =ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initiallized!');
+}
+
+function gotPoses(){
+    if(results.length >0)
+{
+console.log(results);
+leftWristX = results[0].pose.leftWrist.x;
+rightWristX = results[0].pose.rightWrist.x;
+difference = floor(leftWristX - rightWristX);
+
+console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
+
+function draw(){
+    background('#13C3EA');
+    textSize(difference);
+    fill('#F90093');
+    text('Abhishek', 50,300);
+    document.getElementById("font_size").innerHTML = "Width and Height of the text will be: "+difference;
+    
+}
